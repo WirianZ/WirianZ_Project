3c8acc192d8ee6d385727b3de95c23e1fbdcc6f2
WIRIANZ <1wirianz@gmail.com>
2026-01-07 02:18:03 +0700

    Update nara_main.ino

--- Robotic/NARA/nara_main.ino
+++ Robotic/NARA/nara_main.ino
@@ -1,3 +1,13 @@
-// Project : NARA
-// Status : Research & Design
-// To do :
+# 🏺 NARA
+
+"The best time to plant a tree was 20 years ago. The second best time is now."
+
+NARA is a project i created after the failure i have in the past
+
+## Status:
+- [x] Ide Dasar (Cek di folder /docs)
+- [ ] Rancang Bangun Fisik
+- [ ] Kodingan Pergerakan Dasar
+
+Why? :
+  I want to recreate the great idea but poor output
